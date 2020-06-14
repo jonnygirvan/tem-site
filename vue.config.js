c065b873815cc1d74cf46fd31618af4d111151fe
Jonny <jonnygirvan@gmail.com>
2020-06-14 09:57:04 -0400

    compressed images and added pre render

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/about'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
+}

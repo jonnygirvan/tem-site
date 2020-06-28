8c2c9ffcc74ff4a04e370af366b5e63097844e61
Jonny <jonnygirvan@gmail.com>
2020-06-27 23:36:00 -0400

    lots of services

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,52 +42,8 @@
           <img src="/img/smallparts.png" alt="CNC Parts">
         </div>
       </div>
-      <div id="servicesContainer">
-        <div class="servicesColumn">
-          <service title="CNC Milling & Turning" image="/img/services/cnc.svg">
-            <p>Given our competitive advantage of managing all precision CNC milling and turning services in-house, TEM has control over the entire manufacturing process, which allows for quality at each step-from selecting and purchasing the appropriate material to machining and welding the customized end product.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-          <service title="Wire & Vertical EDM" image="/img/services/edm.svg">
-            <p>Our engineering department utilizes CAD/CAM to design precision parts before it moves onto the production floor where our skilled machinists have access to the latest equipment used for electrical discharge machining or Sinker EDM.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-        </div>
-
-        <div class="servicesColumn servicesColumn--shift">
-          <service title="Laser Cutting" image="/img/services/laser.svg">
-            <p>For more than three decades, we have operated multi-axis laser technology for the most difficult aerospace, defense and power generation applications. Over the years we have developed hardware and software features which have become standards in the industry.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-          <service title="Metal Forming" image="/img/services/forming.svg">
-            <p>Qualified machinists who are highly skilled in working with our state-of-the-art metal forming machinery to produce metal formed parts and can work with a variety of materials, including stainless steel, aluminum, and high strength alloys.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-        </div>
-
-        <div class="servicesColumn">
-          <service title="Fabrication" image="/img/services/fabrication.svg">
-            <p>We offer in-house design/build services and can manufacture a wide range of products in any material for numerous industries-from building steel frames for special assembly equipment to parts for aluminum extruding companies, we continually deliver the highest level of customer service.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-          <service title="Defense Parts" image="/img/services/defense.svg">
-            <p>TEM INC. machines parts for the defense and aerospace industries and has been entrusted to machine quality components for highly sensitive applications for companies such as Pratt Whitney and General Dynamics.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-        </div>
-
-        <div class="servicesColumn servicesColumn--shift">
-          <service title="Tool & Die" image="/img/services/tooldie.svg">
-            <p>From simple wrenching tools to complex stamping dies, we can design and build tools to customers’ exact specifications. Our engineers work closely with customers during the tooling design process and consider all aspects that could potentially affect final product quality.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-          <service title="Turbine Manufacturing" image="/img/services/turbine.svg">
-            <p>Whether customers come to TEM INC. with a design or are relying upon our expertise to assist them in the development of a new turbine casting part, they can be assured that the level of service we provide rivals that of other turbine manufacturing companies.</p>
-            <a href="#" class="learnMore">Learn More</a>
-          </service>
-        </div>
-      </div>
     </section>
+      <router-view/>
     <certs></certs>
     <contact></contact>
   </div>
@@ -95,14 +51,13 @@
 
 <script>
 // @ is an alias to /src
-import Service from '@/components/Service'
+// import Service from '@/components/Service'
 import Certs from '@/components/Certs'
 import Contact from '@/components/Contact'
 
 export default {
   name: 'Home',
   components: {
-    Service,
     Certs,
     Contact
   },
@@ -363,7 +318,7 @@ export default {
   }
 
   #servicesContainer {
-    padding: 100px 0 60px;
+    padding: 100px 48px 60px;
     display: flex;
     position: relative;
     flex-wrap: wrap;
---
+++ src/views/Service.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="serviceSingle">
+     <ServiceList></ServiceList>
+     <SingleService title="CNC Milling and Turning" image="/img/services/cnc.png" v-if="$route.params.service == 'cnc'">
+      <p>Our highly skilled and knowledgeable staff of engineers and CNC milling and turning machinists will work with you throughout the design/build and production process to ensure that your product achieves the quality, reliability and durability according to your exact specifications.</p>
+      <h3>Trust the expertise of TEM Inc.</h3>
+      <p>
+        Our precision CNC Vertical and Horizontal Milling Centers and CNC Turning Centers can offer the following:
+      </p>
+      <ul>
+        <li>
+          Capability to work with a wide variety of materials to include but are not limited to aluminum, stainless steel, sheet metal, copper, steel, brass, plastic, titanium and carbide.
+        </li>
+        <li>
+          Our 26,000 sq .ft. CNC machining shop can handle 1-2 part prototypes to 100,000 piece production runs quickly, efficiently and at a competitive price.
+        </li>
+        <li>
+          Customized product design engineering services to assist with either the redesign of an existing part or the development of a new CNC machined part.
+        </li>
+        <li>
+          Skilled machinists using the latest techniques and state-of-the-art CNC milling and turning machines including both 3-axis CNC milling and 4-axis CNC machining.
+        </li>
+        <li>
+          Quality control processes implemented throughout the entire CNC lathe and milling manufacturing process as governed by ISO 9001:2008 and AS9100 certification standards.
+        </li>
+      </ul>
+      <h3>Reliable, precision CNC milling and turning machine shop</h3>
+      <p>With the in-house expertise to handle a multitude of manufacturing projects, TEM. has become a reliable CNC milling and turning machine shop resource from large U.S. corporations to the small local shop. We have been entrusted to develop parts for combat gear made for high strength alloys required by the defense industry to precision CNC machining inspection equipment diaphragms for General Electric, and even producing specialty tooling fixtures requiring small CNC milling and turning for local shops.</p>
+      <p>Our ability to respond to the diverse needs of our customers offers assurance that we can provide them with complete engineering and design solutions at our CNC milling and turning centers that will accommodate their specifications, regardless of the size of their project.</p>
+      <p>Know that TEM is devoted to customer satisfaction and promises to continually provide your company with the highest quality of CNC machining services.</p>
+     </SingleService>
+
+     <SingleService title="Laser Cutting" image="/img/services/laser.jpg" v-if="$route.params.service == 'laser'">
+      <p>Using the Laserdyne BeamDirector System with a KJ704 laser, we employ percussion, trepanning and drill-on-the-fly laser drilling techniques for effusion cooling holes as well as full five-axis laser cutting on pre-formed and machined parts. We can drill holes as small as 0.005 inch in diameter at angles from 15° to 90° degrees to the surface. We can process parts up to 60 inches in diameter and over 48 inches tall.</p>
+      <ul>
+        <li>Setup Time Reductions</li>
+        <li>Faster laser parameter development</li>
+        <li>Better control of recast and surface finish</li>
+        <li>Reduction in production steps</li>
+        <li>Delivers a cost-effective manufacturing solution</li>
+      </ul>
+      <p>At TEM, we deliver consistently smooth, burr-free edges. Accuracies are a tight 0.004 in true position total tolerance for airfoil shapes, cooling holes and trimmed surfaces. Using the Automatic Focus Control (AFC) allows us to maintaining critical focus position while following the contour of the part regardless of slight surface irregularities. With the AFC, all machine axes react to sensing of the part surface, creating unlimited axes corrections with high speed and unmatched sensitivity. AFC allows top machine speeds so productivity is maximized without downtime or scrapped parts.</p>
+      <p>Percussion drilling delivers one or more pulses from the laser to a part while the laser beam and part are stationary. More than one pulse may be required depending on the material type and thickness. A variation of percussion drilling is drill-on-the fly, where pulses are delivered to the part by the stationary laser while the part is rotated. The hole placement is a function of rotational speed and laser pulse frequency. If multiple pulses are required, drill-on-the-fly software developed by LASERDYNE engineers is utilized to synchronize the movement of the part to the laser pulses and ensure that multiple pulses are delivered to the exact location required.</p>
+     </SingleService>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ServiceList from '@/components/serviceList'
+import SingleService from '@/components/SingleService'
+export default {
+  name: 'Service',
+  components: {
+    ServiceList,
+    SingleService
+  },
+  data: function() {
+    return {
+    }
+  },
+  mounted() {
+  },
+
+}
+</script>
+
+<style>
+ #serviceSingle {
+   display: flex;
+   margin-top: 100px
+ }
+
+#serviceList {
+  flex: 0 0 320px;
+}
+
+</style>
